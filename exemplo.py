c9ec0309ac67c43b02e0a6a701da688ebe94beb5
João Bispo dos Santos Filho <138328532+joaobispo23@users.noreply.github.com>
2023-08-05 01:10:54 -0300

    Add files via upload

---
+++ exemplo.py
@@ -0,0 +1,13 @@
+a=10
+b=25
+print("A soma de a e b é:", a +b )
+
+# Este é um caracte somente 
+# Outro Comentario 
+
+x=10
+if (x == 10): 
+    """
+    Esta é a definição de X
+    """
+print ('X é igual a 10 ')
